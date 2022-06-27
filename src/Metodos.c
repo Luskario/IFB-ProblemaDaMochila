092b6d8b9c2e72074a92010ae775268dbc67b452
Lucas <88948009+Luskario@users.noreply.github.com>
2022-06-27 09:01:23 -0300

    Update Metodos.c

--- src/Metodos.c
+++ src/Metodos.c
@@ -9,4 +9,10 @@ void menorPeso(Item *itens, Mochila *mochila){
 
 
 }
-void custoBeneficio(Item *itens, Mochila *mochila);
+void custoBeneficio(Item *itens, Mochila *mochila){
+    
+}
+
+void progDinamica(Item *itens, Mochila *mochila){
+    
+}
