c30cb46ea0c8d581cf15df6fcbd47248787a0010
Lucas <88948009+Luskario@users.noreply.github.com>
2022-06-27 09:39:58 -0300

    Update Metodos.c

--- src/Metodos.c
+++ src/Metodos.c
@@ -2,17 +2,29 @@
 #include <stdlib.h>
 #include "Metodos.h"
 
-void menorPeso(Item *itens, Mochila *mochila){
+void menorPeso(Item *itens, Mochila mochila, int qtd){
+    Item menor = {0, 0};
+    int i, pesoTotal = 0, resposta[qtd];
+    Item ordenado[qtd];
 
-    
+    //ordenar o vetor depois
+
+
+    while(mochila.capacidade > pesoTotal){
+        
+    }
 
 
 
 }
-void custoBeneficio(Item *itens, Mochila *mochila){
+void custoBeneficio(Item *itens, Mochila mochila, int qtd){
     
 }
 
-void progDinamica(Item *itens, Mochila *mochila){
+void progDinamica(Item *itens, Mochila mochila, int qtd){
+    
+}
+
+void criarResposta(Item *itens){
     
 }
